e6d63216503b4710a7ec729e5cf634a1a695872c
ebagabee <ebagabe.2025@gmail.com>
2025-08-23 18:09:12 -0300

    resolve factorial

---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(num):    
+    if num == 0:
+        return 1
+
+    factorial = 1
+    
+    for i in range(1, num + 1):
+        factorial = factorial * i
+    return factorial
+        
